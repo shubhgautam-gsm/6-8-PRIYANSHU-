d4b42db4561f8a9fa266d74dfd4742e837e022cf
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2024-07-30 18:10:29 +0530

    29-07-24

---
+++ PYTHON/p16.py
@@ -0,0 +1,12 @@
+#concat (joint) not total beccause this is two string 
+a='15'
+b=20
+c=a+b
+print(c)
+
+
+#sum ( not joint) but total beccause this is two number  
+a1=10
+b1=20
+d=a1+b1
+print(d)
---
+++ PYTHON/p17.py
@@ -0,0 +1,8 @@
+try:
+ a = int(input("Enter a:"))
+ b = int(input("Enter b:"))
+ c = a/b
+ 
+except:
+ print("Can't divide with zero")
+ 
